0ed8dbdbe7ae25209b1f458f4d211e64bfbb44b2
Duarte-753 <julioduartebatista753@gmail.com>
2024-02-10 17:05:39 -0300

    Alteração em tamanho do caracterer no  modelo e placeholder das label

--- SistemaBoletimTransporteDigital/Models/UsuarioModel.cs
+++ SistemaBoletimTransporteDigital/Models/UsuarioModel.cs
@@ -9,23 +9,29 @@ namespace SistemaBoletimTransporteDigital.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage ="Digite o Código Funcional do funcionario")] //Faz o Campo de baixo ser obrigatório
+        [MaxLength(20)]
         public string CodigoFuncional { get; set; }
 
         [Required(ErrorMessage = "Digite o Nome do funcionario")]
+        [MaxLength(40)]
         public string Nome { get; set; }
 
         [Required(ErrorMessage ="Digite o nome de Usuário")]
+        [MaxLength(20)]
         public string Usuario { get; set; }
 
         [Required(ErrorMessage ="Digite a senha desse Usuário")]
+        [MaxLength(20)]
         public string Senha { get; set; }
 
         [Required(ErrorMessage = "Digite um email para recuperação de senha.")]
         [EmailAddress(ErrorMessage ="O e-mail informado não é valido!")]
+        [MaxLength(40)]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Digite o celular para contato.")]
         [Phone(ErrorMessage = "O celular informado não é valido.")]
+        [MaxLength(11)]
         public string Celular { get; set; }
 
         [Required(ErrorMessage = "Selecione o Perfil do Usuário.")]
