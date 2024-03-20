3539b4bcae2f1c846669269f88171ee3909760e5
Lara Ledier <larabeatrizledier@gmail.com>
2024-03-20 19:58:32 -0300

    Arrumando manutencao

--- SistemaBoletimTransporteDigital/Migrations/20240320225325_CriacaoColunamanutencao.Designer.cs
+++ SistemaBoletimTransporteDigital/Migrations/20240320225325_CriacaoColunamanutencao.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaBoletimTransporteDigital.Data;
 
@@ -11,9 +12,11 @@ using SistemaBoletimTransporteDigital.Data;
 namespace SistemaBoletimTransporteDigital.Migrations
 {
     [DbContext(typeof(BancoContext))]
-    partial class BancoContextModelSnapshot : ModelSnapshot
+    [Migration("20240320225325_CriacaoColunamanutencao")]
+    partial class CriacaoColunamanutencao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
