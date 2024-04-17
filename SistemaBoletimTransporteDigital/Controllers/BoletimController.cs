ebe20e5729de5c75389ed2c7dcbe22e83cc7ef0e
Duarte-753 <julioduartebatista753@gmail.com>
2024-04-17 11:09:08 -0300

    Precisa ver o boletim view, controller e viewModel

--- SistemaBoletimTransporteDigital/Controllers/BoletimController.cs
+++ SistemaBoletimTransporteDigital/Controllers/BoletimController.cs
@@ -30,23 +30,14 @@ namespace SistemaBoletimTransporteDigital.Controllers
             // Realizar a consulta no banco de dados usando as datas fornecidas
             var corridas = await _bancoContext.Corridas
                 .Where(c => c.DataInicioCorrida >= model.Filtros.DataInicial && c.DataFinalCorrida <= model.Filtros.DataFinal)
-                .ToListAsync();
+                .Select(c => new BoletimViewModel
+                {
+               
+                }).ToListAsync();
 
-            // Mapear os resultados para a ViewModel e enviar para a View
-            var dadosViewModel = corridas.Select(c => new DadosViewModel
-            {
-                Id = c.Id,
-                DataInicioCorrida = c.DataInicioCorrida,
-                DataFinalCorrida = c.DataFinalCorrida,
-                DescricaoCorrida = c.DescricaoCorrida,
-                KmInicial = c.KmInicial,
-                KmFinal = c.KmFinal,
-                //Placa = c.Veiculo.Placa,
-                //Celular = c.Usuario.Celular,
-                //Qtd = c.VeiculosDisponiveis.Count()
-            }).ToList();
-
-            return View(dadosViewModel);
+           
+                return View(corridas);
+            
         }
     }
 }
