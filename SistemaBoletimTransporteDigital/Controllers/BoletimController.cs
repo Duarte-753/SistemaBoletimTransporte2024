277c29e0e2fc2a7b5c8db336430d25472f4fdd0a
Duarte-753 <julioduartebatista753@gmail.com>
2024-04-17 15:07:23 -0300

    Funcionando filtro sobre corridas selecionando as corridas por data e do usuario logado

--- SistemaBoletimTransporteDigital/Controllers/BoletimController.cs
+++ SistemaBoletimTransporteDigital/Controllers/BoletimController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SistemaBoletimTransporteDigital.Data;
+using SistemaBoletimTransporteDigital.Helper;
+using SistemaBoletimTransporteDigital.Models;
+using SistemaBoletimTransporteDigital.Repositorio;
 using SistemaBoletimTransporteDigital.ViewModels;
 
 namespace SistemaBoletimTransporteDigital.Controllers
@@ -8,14 +11,24 @@ namespace SistemaBoletimTransporteDigital.Controllers
     public class BoletimController : Controller
     {
         private readonly BancoContext _bancoContext;
+        private readonly ICorridaRepositorio _corridaRepositorio;       
+        private readonly ISessao _sessao;
+        private readonly IVeiculoRepositorio _veiculoRepositorio;
+        
 
-        public BoletimController(BancoContext bancoContext)
+        public BoletimController(ICorridaRepositorio corridaRepositorio, ISessao sessao, IVeiculoRepositorio veiculoRepositorio, BancoContext bancoContext)
         {
             _bancoContext = bancoContext;
+            _corridaRepositorio = corridaRepositorio;
+            _sessao = sessao;
+            _veiculoRepositorio = veiculoRepositorio;                     
         }
 
         public async Task<IActionResult> Index(BoletimViewModel model)
         {
+            var usuarioLogado = _sessao.BuscarSessaoDoUsuario();
+            var corridasUsuario = _corridaRepositorio.BuscarCorrida(usuarioLogado.Id);
+
             // Definir datas padrão caso não tenham sido fornecidas
             if (!model.Filtros.DataInicial.HasValue)
             {
@@ -29,25 +42,31 @@ namespace SistemaBoletimTransporteDigital.Controllers
 
             // Realizar a consulta no banco de dados usando as datas fornecidas
             var corridas = await _bancoContext.Corridas
-                .Where(c => c.DataInicioCorrida >= model.Filtros.DataInicial && c.DataFinalCorrida <= model.Filtros.DataFinal)
-                .Select(c => new DadosViewModel
-                {
-                    Id = c.Id,
-                    DataInicioCorrida = c.DataInicioCorrida,
-                    DataFinalCorrida = c.DataFinalCorrida,
-                    DescricaoCorrida = c.DescricaoCorrida,
-                    KmInicial = c.KmInicial,
-                    KmFinal = c.KmFinal
-                }).ToListAsync();
+                 .Where(c => c.DataInicioCorrida >= model.Filtros.DataInicial && c.DataFinalCorrida <= model.Filtros.DataFinal &&
+                      c.UsuarioID == usuarioLogado.Id
+                ).ToListAsync();       
 
             var viewModel = new BoletimViewModel
             {
                 Filtros = model.Filtros,
-                Dados = model.Dados,
-                
+                Dados = corridas// Usar os resultados da consulta como dados
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            UsuarioModel usuarioLogado = _sessao.BuscarSessaoDoUsuario();
+            List<CorridaModel> corridas = _corridaRepositorio.BuscarCorrida(usuarioLogado.Id); // buscando somente a corrida do usuario
+            //var dropdown = _veiculoRepositorio.BuscarVeiculos();
+            //ViewBag.VeiculosDisponiveis = new SelectList(dropdown, "Id", "Veiculo");
+            var corrida = new CorridaModel();
+            corrida.VeiculosDisponiveis = _veiculoRepositorio.BuscarVeiculos();
+
+
+            return View(corridas);
         }
 
     }
