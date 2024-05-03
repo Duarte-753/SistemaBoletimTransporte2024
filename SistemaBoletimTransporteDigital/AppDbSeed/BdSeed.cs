416c1f0ed8d4703419463c4a99182121f4f77070
Duarte-753 <julioduartebatista753@gmail.com>
2024-05-02 21:37:41 -0300

    Feito alteração no SEED

--- SistemaBoletimTransporteDigital/AppDbSeed/BdSeed.cs
+++ SistemaBoletimTransporteDigital/AppDbSeed/BdSeed.cs
@@ -31,42 +31,42 @@ namespace SistemaBoletimTransporteDigital.AppDbSeed
                     // Outras propriedades
                 }
             );
-            modelBuilder.Entity<UsuarioModel>().HasData(
-                new UsuarioModel
-                {
-                    Id = 2,
-                    CodigoFuncional = 567,
-                    Nome = "motorista",
-                    Usuario = "MOTORISTA",
-                    Senha = "a61e38f3910fba1d8e1fb97f4b3561df07ab0d81",
-                    Email = "julioduartebatista753@gmail.com",
-                    Celular = "11912345678",
-                    Perfil = Enums.PerfilEnum.motorista,
-                    CorridaStatus = Enums.PerfilEnum.Finalizada,
-                    EstaVinculadoAumaCorrida = Enums.PerfilEnum.VinculadoAcorridaNao,
-                    DataCriacao = DateTime.Now,
-                    DataUltimaAtualizacao = DateTime.Now,
-                    // Outras propriedades
-                }
-            );
-            modelBuilder.Entity<UsuarioModel>().HasData(
-               new UsuarioModel
-               {
-                   Id = 3,
-                   CodigoFuncional = 9876,
-                   Nome = "motorista2",
-                   Usuario = "MOTORISTA2",
-                   Senha = "b739522c59a564437fc8c6ad639176f704766596",
-                   Email = "julioduartebatista753@gmail.com",
-                   Celular = "11912345678",
-                   Perfil = Enums.PerfilEnum.motorista,
-                   CorridaStatus = Enums.PerfilEnum.Finalizada,
-                   EstaVinculadoAumaCorrida = Enums.PerfilEnum.VinculadoAcorridaNao,
-                   DataCriacao = DateTime.Now,
-                   DataUltimaAtualizacao = DateTime.Now,
-                   // Outras propriedades
-               }
-           );
+            //modelBuilder.Entity<UsuarioModel>().HasData(
+            //    new UsuarioModel
+            //    {
+            //        Id = 2,
+            //        CodigoFuncional = 567,
+            //        Nome = "motorista",
+            //        Usuario = "MOTORISTA",
+            //        Senha = "a61e38f3910fba1d8e1fb97f4b3561df07ab0d81",
+            //        Email = "julioduartebatista753@gmail.com",
+            //        Celular = "11912345678",
+            //        Perfil = Enums.PerfilEnum.motorista,
+            //        CorridaStatus = Enums.PerfilEnum.Finalizada,
+            //        EstaVinculadoAumaCorrida = Enums.PerfilEnum.VinculadoAcorridaNao,
+            //        DataCriacao = DateTime.Now,
+            //        DataUltimaAtualizacao = DateTime.Now,
+            //        // Outras propriedades
+            //    }
+            //);
+           // modelBuilder.Entity<UsuarioModel>().HasData(
+           //    new UsuarioModel
+           //    {
+           //        Id = 3,
+           //        CodigoFuncional = 9876,
+           //        Nome = "motorista2",
+           //        Usuario = "MOTORISTA2",
+           //        Senha = "b739522c59a564437fc8c6ad639176f704766596",
+           //        Email = "julioduartebatista753@gmail.com",
+           //        Celular = "11912345678",
+           //        Perfil = Enums.PerfilEnum.motorista,
+           //        CorridaStatus = Enums.PerfilEnum.Finalizada,
+           //        EstaVinculadoAumaCorrida = Enums.PerfilEnum.VinculadoAcorridaNao,
+           //        DataCriacao = DateTime.Now,
+           //        DataUltimaAtualizacao = DateTime.Now,
+           //        // Outras propriedades
+           //    }
+           //);
             // Adicione mais usuários conforme necessário
         }
 
@@ -77,9 +77,9 @@ namespace SistemaBoletimTransporteDigital.AppDbSeed
                 {
                     Id = 1,
                     Prefixo = 12345,
-                    Veiculo = "Golf",
-                    Cor = "Branco",
-                    Placa = "FWF-1232",
+                    Veiculo = "GOLF GTI",
+                    Cor = "BRANCO",
+                    Placa = "FWF1232",
                     Quilometragem = 12600,
                     Ano = 2014,
                     Valor = 259875,
@@ -95,9 +95,9 @@ namespace SistemaBoletimTransporteDigital.AppDbSeed
                {
                    Id = 2,
                    Prefixo = 678910,
-                   Veiculo = "Fiat Uno",
-                   Cor = "Branco",
-                   Placa = "ASD-2345",
+                   Veiculo = "FIAT UNO SPORT",
+                   Cor = "PRETO",
+                   Placa = "ASD2Y45",
                    Quilometragem = 450067,
                    Ano = 2016,
                    Valor = 15000,
@@ -108,23 +108,23 @@ namespace SistemaBoletimTransporteDigital.AppDbSeed
                }
            );
 
-            modelBuilder.Entity<VeiculoModel>().HasData(
-               new VeiculoModel
-               {
-                   Id = 3,
-                   Prefixo = 121235,
-                   Veiculo = "Palio Weekend",
-                   Cor = "Azul",
-                   Placa = "JHF-7653",
-                   Quilometragem = 100,
-                   Ano = 2024,
-                   Valor = 45000,
-                   CadastroSistema = DateTime.Now,
-                   DataUltimaAtualizacao = DateTime.Now,
-                   CarroEmUso = Enums.CarroEmUsoEnum.Disponivel,
-                   // Popule outras propriedades
-               }
-           );
+            // modelBuilder.Entity<VeiculoModel>().HasData(
+            //    new VeiculoModel
+            //    {
+            //        Id = 3,
+            //        Prefixo = 121235,
+            //        Veiculo = "Palio Weekend",
+            //        Cor = "Azul",
+            //        Placa = "JHF-7653",
+            //        Quilometragem = 100,
+            //        Ano = 2024,
+            //        Valor = 45000,
+            //        CadastroSistema = DateTime.Now,
+            //        DataUltimaAtualizacao = DateTime.Now,
+            //        CarroEmUso = Enums.CarroEmUsoEnum.Disponivel,
+            //        // Popule outras propriedades
+            //    }
+            //);
             // Adicione mais veículos conforme necessário
         }
         
